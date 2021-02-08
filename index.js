f74140f5b3be6bf1642f8058d3af5a58fb629830
geenva <geenva@users.noreply.github.com>
2021-02-08 22:08:21 +0800

    🚀 | Vercel quick fx

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 
-router.get("/", function (req, res) {
+router.get("/api", function (req, res) {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
