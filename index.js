de3bac7939ddeacef374e4b1336b113536cf68df
geenva <geenva@users.noreply.github.com>
2021-02-08 21:58:49 +0800

    ✨ | Homepage

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+const router = express.Router();
+
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.use("/", router);
+app.listen(process.env.port || "8080");
