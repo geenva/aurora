ed52cf57f1a563024b89bf7bc180028a8f657b1c
geenva <geenva@users.noreply.github.com>
2021-02-09 16:00:21 +0800

    ✨ | API!

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
+router.get("/api", (req, res, next) => {
+  console.log("Serving API to user.");
+  res.json({
+    status: "200",
+    content:
+      "Yes, this is working! But you see, we're not quite sure what to do with this API just yet. See you soon, in the future!",
+  });
+});
+
 app.use("/", router);
 app.listen(process.env.port || "8080");
 console.log("Listening, loud and clear.");
